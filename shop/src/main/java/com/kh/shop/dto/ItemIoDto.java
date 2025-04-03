174f723dc441a37e3464bf609ce5e862cb637ae2
ssw4111 <sswoo4111@gmail.com>
2025-04-03 16:37:46 +0900

    io dto mapper 흥

---
+++ shop/src/main/java/com/kh/shop/dto/ItemIoDto.java
@@ -0,0 +1,23 @@
+package com.kh.shop.dto;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+public class ItemIoDto {
+	private int itemIoNo; //itemNo
+	private int itemIoTotal;
+	private int itemIoIn;
+	private Timestamp itemIoInTime;
+	private int itemIoOut;
+	private Timestamp itemIoOutTIme;
+	
+	public int getTotal() {
+	  return itemIoIn - itemIoOut;
+	    }
+	
+}
